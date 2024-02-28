11d98abef689f897d98ee2b01198955ebc27b09c
EduardoNicoleit <eduardonicoleit@hotmail.com>
2024-02-28 10:42:03 -0300

    main

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,27 @@
-// SPDX-License-Identifier: MIT
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-LIcense-Identifier: MIT
 
+// aa
 pragma solidity ^0.8.18;
 
 /**
@@ -11,15 +32,26 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
+    error NotEnoughEthSent();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent"); -> more costly
+        if (msg.value < i_entranceFee) {
+            revert NotEnoughEthSent();
+        }
+    }
 
     function pickWinner() public {}
 
-    function aaaa() public {}
+    //** Getter Function */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
