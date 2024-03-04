b1147739dca0c4b88bac80f95e931741d0345b4e
EduardoNicoleit <eduardonicoleit@hotmail.com>
2024-03-04 16:09:10 -0300

    main

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,5 +2,8 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
 
-contract DeployRaffle is Script {}
+contract DeployRaffle is Script {
+    function run() external returns (Raffle) {}
+}
